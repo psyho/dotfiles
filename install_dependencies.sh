0fca2f9eec3dd835fed9887df1638e6e1997f1b2
Adam Pohorecki <adam.pohorecki@llp.pl>
2010-08-31 19:15:55 +0200

    initial, empty vim config

---
+++ install_dependencies.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+##################################
+# Various Dependencies
+##################################
+
+sudo apt-get install vim vim-gtk # vim and gvim 
+sudo apt-get install ttf-inconsolata # font for programming
---
+++ link_configs.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+import os
+import os.path
+
+def current_dir():
+  path = os.path.dirname(__file__)
+  return os.path.abspath(path)
+
+def link_local_to_home(local_name, home_name):
+  local_path = os.path.join(current_dir(), local_name)
+  home_path = os.path.expanduser(home_name)
+  
+  if os.path.exists(home_path): os.remove(home_path)
+  os.symlink(local_path, home_path)
+
+link_local_to_home('vim', '.vim')
+link_local_to_home('vimrc', '.vimrc')
+link_local_to_home('gvimrc', '.gvimrc')
