65ac4990af368b03e15e8c5aadc197922d41f5c2
Adam Pohorecki <adam.pohorecki@llp.pl>
2010-09-06 13:08:18 +0200

    I think that vim-ruby uses exuberant-ctags

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -6,4 +6,5 @@
 
 sudo apt-get install vim vim-gtk # vim and gvim 
 sudo apt-get install ttf-inconsolata # font for programming
-sudo apt-get install ack-grep # for ack.vim
+sudo apt-get install ack-grep # for ack.vim 
+sudo apt-get install exuberant-ctags
