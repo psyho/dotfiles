6b7785fab1b52b36f3aace60d886864ee8137445
Adam Pohorecki <adam.pohorecki@llp.pl>
2010-09-07 15:39:38 +0200

    ack is not really ack

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -6,5 +6,5 @@
 
 sudo apt-get install vim vim-gtk # vim and gvim 
 sudo apt-get install ttf-inconsolata # font for programming
-sudo apt-get install ack-grep ack # for ack.vim 
+sudo apt-get install ack-grep # for ack.vim 
 sudo apt-get install exuberant-ctags
