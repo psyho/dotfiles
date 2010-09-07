7a925555cf7fe240de23d2d226d797c4ef1c2d81
Adam Pohorecki <adam.pohorecki@llp.pl>
2010-09-07 15:45:10 +0200

    Symbolic link to ack must be created.

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -7,4 +7,5 @@
 sudo apt-get install vim vim-gtk # vim and gvim 
 sudo apt-get install ttf-inconsolata # font for programming
 sudo apt-get install ack-grep # for ack.vim 
+sudo ln -s /usr/bin/ack-grep /usr/local/bin/ack # ack-grep won't cut it for ack.vim
 sudo apt-get install exuberant-ctags
